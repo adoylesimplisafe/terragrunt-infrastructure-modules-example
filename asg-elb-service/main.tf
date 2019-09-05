0db317f25e92f2c59c830819a3accf4ea70edccf
Andrew Doyle <andrew.doyle@simplisafe.com>
2019-09-05 12:13:11 -0400

    quick fixypoo

--- asg-elb-service/main.tf
+++ asg-elb-service/main.tf
@@ -26,10 +26,12 @@ terraform {
 
 resource "aws_autoscaling_group" "webserver_example" {
   launch_configuration = aws_launch_configuration.webserver_example.id
-  availability_zones   = var.aws_vpc_azs
+  availability_zones   = data.aws_availability_zones.all.names
 
   load_balancers    = [aws_elb.webserver_example.name]
   health_check_type = "ELB"
+
+  vpc_zone_identifier  = var.aws_vpc_zone_ids
 
   min_size = var.min_size
   max_size = var.max_size
@@ -40,6 +42,8 @@ resource "aws_autoscaling_group" "webserver_example" {
     propagate_at_launch = true
   }
 }
+
+data "aws_availability_zones" "all" {}
 
 # ---------------------------------------------------------------------------------------------------------------------
 # CREATE THE LAUNCH CONFIGURATION
@@ -96,7 +100,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_elb" "webserver_example" {
   name               = var.name
-  availability_zones = var.aws_vpc_azs
+  availability_zones = data.aws_availability_zones.all.names
   security_groups    = ["${var.elb_security_group_id}"]
 
   listener {
